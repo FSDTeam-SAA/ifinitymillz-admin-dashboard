c448fee765e45b0e2b8c4c517f33efb85b48f71f
Tanvir Ahmmed <ahmmedtanvir009@gmail.com>
2026-04-18 14:55:50 +0600

    add

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(request: NextRequest) {
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  const { pathname } = request.nextUrl;
+
+  const isPublicRoute = pathname === "/signin";
+
+  // ❌ not logged in → redirect to signin
+  if (!token && !isPublicRoute) {
+    return NextResponse.redirect(new URL("/signin", request.url));
+  }
+
+  // ✅ logged in → prevent going to signin
+  if (token && isPublicRoute) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
